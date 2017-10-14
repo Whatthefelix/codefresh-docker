1a6127cce103f9cf20564e2abca2e79210742a40
Felix Feng <felixisfeng@gmail.com>
2017-10-14 11:01:51 -0700

    initail commit for go testing out codefresh

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "runtime"
+)
+
+// RW = HTTP Response Writer
+func helloHandler(rw http.ResponseWriter, r *http.Request) {
+    // FMT = format writing
+  fmt.Fprintf(rw, `
+      <h1>Hello World!</h1>
+      <p>Greetings from %s with an %s CPU.
+      <p>Check out the <a href="https://github.com/ogryzek/hello_go">source code</a>.
+      `, runtime.GOOS, runtime.GOARCH)
+}
+
+func main(){
+  http.HandleFunc("/", helloHandler)
+  http.ListenAndServe(":3000", nil)
+}
